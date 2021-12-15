4177cc6dbfa1f8ceb4ce7d2c43aae02643c27911
Ivan Vnucec <ivnucec@example.com>
2021-12-15 13:05:34 +0100

    init

---
+++ app/src/main/java/com/example/biblija_ks/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.biblija_ks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.biblija_ks.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'biblija_ks' library on application startup.
+    static {
+        System.loadLibrary("biblija_ks");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'biblija_ks' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
