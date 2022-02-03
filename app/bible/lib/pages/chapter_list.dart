151b19c903f3f36124d3edf4ec90a79d904eb669
Ivan Vnučec <vnucec.ivan@gmail.com>
2022-02-03 15:41:19 +0100

    add dummy chapters list

--- app/bible/lib/pages/chapter_list.dart
+++ app/bible/lib/pages/chapter_list.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'package:bible/res/colors.dart';
+import 'package:bible/widgets/list_tile.dart';
 import 'package:flutter/material.dart';
 
 class ChaptersList extends StatefulWidget {
@@ -15,12 +19,27 @@ class _ChaptersListState extends State<ChaptersList> {
     // TODO: get files list
     return Scaffold(
       appBar: AppBar(
-        // TODO: add New Testament or old testament title text
-        title: const Text('New Testament/Old Testament'),
-      ),
+          // TODO: add New Testament or old testament title text
+          title: const Text('New Testament/Old Testament'),
+          foregroundColor: MyColors.appBarForegroundColor,
+          backgroundColor: MyColors.testamentButtonBackgroundColors[0]),
       body: Center(
-        child: Text(widget.bookPath),
+        child: ListView(
+          children: [
+            TestamentBookListTile("Glava 1", "", (String book_path) {}),
+            TestamentBookListTile("Glava 2", "", (String book_path) {}),
+            TestamentBookListTile("Glava 3", "", (String book_path) {}),
+          ],
         ),
-      );
+      ),
+      backgroundColor: MyColors.homeBackgroundColor,
+    );
+  }
+
+  // TODO: test me
+  Future<Iterable<File>> getListOfFilesInDir(String path) async {
+    final dir = Directory(path);
+    final List<FileSystemEntity> entities = await dir.list().toList();
+    return entities.whereType<File>();
   }
 }
